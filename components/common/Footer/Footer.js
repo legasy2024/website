0e1142c420c66b59e49ea1d00f5685ea14c5ad98
Andrés Poveda <81783473+apovedq@users.noreply.github.com>
2025-05-19 11:15:09 -0500

    Update

--- components/common/Footer/Footer.js
+++ components/common/Footer/Footer.js
@@ -11,14 +11,7 @@ function Footer({ translations }) {
     <footer className="flex flex-col items-center w-full bg-TextBlack py-8 px-5 m-0 desktop:px-24">
       <div className="flex flex-col gap-y-8 items-center justify-around w-full md:flex-row desktop:max-w-[1200px] desktop:flex-row md:border-t-2 border-white/20 py-5">
         <Image src={logo} width={140} height={40} alt="Logo image" />
-        <span className="flex flex-row gap-x-3">
-          <p className="text-white text-sm underline">
-            {translations.terms}
-          </p>
-          <p className="text-white text-sm underline">
-            {translations.privacy}
-          </p>
-        </span>
+       
         <span className="flex flex-row gap-x-3">
           <a href="https://www.instagram.com/legassystudio/" target="_blank" className="cursor-pointer" rel="noopener noreferrer">
             <figure className="p-4 bg-white/10 rounded-full">
